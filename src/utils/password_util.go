2448d9d9c44161446a7b4f191421d316d2587c6a
Lorenzo DZ <lorenzo.dz@hotmail.com>
2025-03-22 22:03:50 -0300

    Implements comprehensive password management utilities

--- src/utils/password_util.go
+++ src/utils/password_util.go
@@ -1,37 +1,43 @@
 package utils
 
 import (
+	"crypto/rand"
+	"encoding/base64"
 	"errors"
 	"fmt"
+	"math/big"
+	"strings"
+
+	"golang.org/x/crypto/bcrypt"
 )
 
-// Definicao de constantes para senhas
+// Definition of constants for passwords
 const (
-	// Custo padrao para o algoritmo bcrypt
+	// DefaultBcryptCost is the default cost for the bcrypt algorithm
 	DefaultBcryptCost = 12
-	// Minimo de caracteres para a senha
+	// MinPasswordLength is the minimum password length
 	MinPasswordLength = 8
-	// Maximo de caracteres para a senha
+	// MaxPasswordLength is the maximum password length
 	MaxPasswordLength = 72
-	// Tamanho do codigo numerico
+	// NumericCodeLength is the default length for numeric codes (SMS, WhatsApp)
 	NumericCodeLength = 6
 )
 
 var (
-	// ErrPasswordTooShort indica que a senha é muito curta
+	// ErrPasswordTooShort indicates that the password is too short
 	ErrPasswordTooShort = fmt.Errorf("password must be at least %d characters", MinPasswordLength)
-	// ErrPasswordTooLong indica que a senha é muito longa
+	// ErrPasswordTooLong indicates that the password is too long
 	ErrPasswordTooLong = fmt.Errorf("password must be at most %d characters", MaxPasswordLength)
-	// ErrPasswordTooWeak indica que a senha é muito fraca
+	// ErrPasswordTooWeak indicates that the password is too weak
 	ErrPasswordTooWeak = errors.New("password is too weak, it should include uppercase, lowercase, numbers and special characters")
 )
 
-// PasswordUtil fornece funcoes para trabalhar com senhas
+// PasswordUtil provides functions for working with passwords
 type PasswordUtil struct {
 	BcryptCost int
 }
 
-// NewPasswordUtil cria uma nova instancia de PasswordUtil
+// NewPasswordUtil creates a new instance of PasswordUtil
 func NewPasswordUtil(bcryptCost int) *PasswordUtil {
 	if bcryptCost <= 0 {
 		bcryptCost = DefaultBcryptCost
@@ -41,5 +47,127 @@ func NewPasswordUtil(bcryptCost int) *PasswordUtil {
 	}
 }
 
+// ValidatePasswordLength validates the password length
+func (p *PasswordUtil) ValidatePasswordLength(password string) error {
+	if len(password) < MinPasswordLength {
+		return ErrPasswordTooShort
+	}
+	if len(password) > MaxPasswordLength {
+		return ErrPasswordTooLong
+	}
+	return nil
+}
 
+// HashPassword generates a bcrypt hash for the password
+func (p *PasswordUtil) HashPassword(password string) (string, error) {
+	// We validate the password length
+	if err := p.ValidatePasswordLength(password); err != nil {
+		return "", err
+	}
 
+	hash, err := bcrypt.GenerateFromPassword([]byte(password), p.BcryptCost)
+	if err != nil {
+		return "", err
+	}
+
+	return string(hash), nil
+}
+
+// VerifyPassword checks if the password matches the hash
+func (p *PasswordUtil) VerifyPassword(hashedPassword, password string) error {
+	return bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(password))
+}
+
+// ValidatePasswordStrength checks if the password is strong enough
+func (p *PasswordUtil) ValidatePasswordStrength(password string) error {
+	if err := p.ValidatePasswordLength(password); err != nil {
+		return err
+	}
+
+	var (
+		hasUpper   bool
+		hasLower   bool
+		hasNumber  bool
+		hasSpecial bool
+	)
+
+	for _, char := range password {
+		switch {
+		case 'A' <= char && char <= 'Z':
+			hasUpper = true
+		case 'a' <= char && char <= 'z':
+			hasLower = true
+		case '0' <= char && char <= '9':
+			hasNumber = true
+		case strings.ContainsRune("!@#$%^&*()-_[]{}|;:,.<>?/", char):
+			hasSpecial = true
+		}
+	}
+
+	// We require at least 3 of the 4 character types
+	score := 0
+	if hasUpper {
+		score++
+	}
+	if hasLower {
+		score++
+	}
+	if hasNumber {
+		score++
+	}
+	if hasSpecial {
+		score++
+	}
+
+	if score < 3 {
+		return ErrPasswordTooWeak
+	}
+
+	return nil
+}
+
+// GenerateRandomToken generates a random token
+func (p *PasswordUtil) GenerateRandomToken(length int) (string, error) {
+	if length <= 0 {
+		length = 32
+	}
+
+	// Generate random bytes
+	b := make([]byte, length)
+	if _, err := rand.Read(b); err != nil {
+		return "", err
+	}
+
+	// Encode in base64 and remove problematic characters
+	token := base64.URLEncoding.EncodeToString(b)
+	token = strings.ReplaceAll(token, "+", "")
+	token = strings.ReplaceAll(token, "/", "")
+	token = strings.ReplaceAll(token, "=", "")
+
+	// Truncate to the desired length
+	if len(token) > length {
+		token = token[:length]
+	}
+
+	return token, nil
+}
+
+// GenerateNumericCode generates a random numeric code (for SMS, WhatsApp)
+func (p *PasswordUtil) GenerateNumericCode(length int) (string, error) {
+	if length <= 0 {
+		length = NumericCodeLength
+	}
+
+	// Generate random digits
+	var sb strings.Builder
+	for i := 0; i < length; i++ {
+		// Generate a number between 0 and 9
+		num, err := rand.Int(rand.Reader, big.NewInt(10))
+		if err != nil {
+			return "", err
+		}
+		sb.WriteString((num.String()))
+	}
+
+	return sb.String(), nil
+}
